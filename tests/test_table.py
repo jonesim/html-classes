9d29d212e474483198d792d66432a873e2e7e205
jonesim <ian@cobi.co.uk>
2024-06-05 14:08:04 +0100

    Add some html table tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -6,5 +6,20 @@ from html_classes.html import HtmlTable
 class TestTable(unittest.TestCase):
 
     def test_table(self):
-        self.assertEqual(HtmlTable(data=[[1,2],[3, 4]]).render(),
-                '<table><tr></tr><tr><td>1</td><td>2</td></tr><tr></tr><tr><td>3</td><td>4</td></tr></table>')
+        self.assertEqual(HtmlTable(data=[[1, 2],[3, 4]]).render(),
+                         '<table><tr><td>1</td><td>2</td></tr><tr><td>3</td><td>4</td></tr></table>')
+
+
+    def test_table_header(self):
+        self.assertEqual(HtmlTable(data=[[1, 2],[3, 4]], headers=1).render(),
+                         '<table><tr><th>1</th><th>2</th></tr><tr><td>3</td><td>4</td></tr></table>')
+
+    def test_table_header_grouped(self):
+        self.assertEqual(HtmlTable(data=[[1, 2],[3, 4]], headers=1, grouped=True).render(),
+                         '<table><thead><tr><th>1</th><th>2</th></tr></thead>'
+                         '<tbody><tr><td>3</td><td>4</td></tr></tbody></table>')
+
+    def test_table_double_header_grouped(self):
+        self.assertEqual(HtmlTable(data=[[1, 2],[3, 4]], headers=1, left_headers=1, grouped=True).render(),
+                         '<table><thead><tr><th>1</th><th>2</th></tr></thead>'
+                         '<tbody><tr><th>3</th><td>4</td></tr></tbody></table>')
