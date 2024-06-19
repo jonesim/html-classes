098767f3eeb04ac94e126a5217ecad4ab5875a08
jonesim <ian@cobi.co.uk>
2024-06-19 18:21:34 +0100

    Add new tests for HtmlTable

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,6 @@
 import unittest
 
-from html_classes.html import HtmlTable
+from html_classes.html import HtmlTable, HtmlTr, HtmlTd
 
 
 class TestTable(unittest.TestCase):
@@ -23,3 +23,11 @@ class TestTable(unittest.TestCase):
         self.assertEqual(HtmlTable(data=[[1, 2],[3, 4]], headers=1, left_headers=1, grouped=True).render(),
                          '<table><thead><tr><th>1</th><th>2</th></tr></thead>'
                          '<tbody><tr><th>3</th><td>4</td></tr></tbody></table>')
+
+    def test_table_html_row(self):
+        self.assertEqual(HtmlTable(data=[HtmlTr(['1', '2']),[3, 4]],).render(),
+                         '<table><tr><td>1</td><td>2</td></tr><tr><td>3</td><td>4</td></tr></table>')
+
+    def test_table_html_td(self):
+        self.assertEqual(HtmlTable(data=[['1', HtmlTd('2', css_classes='cls')],[3, 4]],).render(),
+                         '<table><tr><td>1</td><td class="cls">2</td></tr><tr><td>3</td><td>4</td></tr></table>')
